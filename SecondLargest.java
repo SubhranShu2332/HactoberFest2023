29d349b3d4c1d26cd099ac42aa610c04e92f4c7d
SubhranShu2332 <124662904+SubhranShu2332@users.noreply.github.com>
2023-10-12 00:37:05 +0530

    Reduced The Time Complexity To O(n)
    Taken Two Variable one for largest another for second largest Then Traverse Through The Array So it takes O(n) In place the previous which takes O(n*logn) because Sorting was There

--- SecondLargest.java
+++ SecondLargest.java
@@ -2,50 +2,48 @@
 // element in an array
 import java.util.*;
 class Secondlargest{
-
-// Function to print the
-// second largest elements
-static void print2largest(int arr[],
-						int arr_size)
-{
-int i, first, second;
-
-// There should be
-// atleast two elements
-if (arr_size < 2)
-{
-	System.out.printf(" Invalid Input ");
-	return;
-}
-
-// Sort the array
-Arrays.sort(arr);
-
-// Start from second last element
-// as the largest element is at last
-for (i = arr_size - 2; i >= 0; i--)
-{
-	// If the element is not
-	// equal to largest element
-	if (arr[i] != arr[arr_size - 1])
+	// Function to print the
+	// second largest elements
+	static void print2largest(int arr[],int arr_size)
 	{
-	System.out.printf("The second largest " +
-						"element is %d\n", arr[i]);
-	return;
+		int i, first, second;
+		// There should be
+		// atleast two elements
+		if (arr_size < 2)
+		{
+			System.out.printf(" Invalid Input ");
+			return;
+		}
+		first = second = Integer.MIN_VALUE; 
+		for (i = 0; i < arr_size; i++) 
+		{ 
+			/* If current element is greater than  
+		        first then update both first and second */
+			if (arr[i] > first) 
+			{ 
+		        	second = first; 
+		                first = arr[i]; 
+		        } 
+		  
+		        /* If arr[i] is in between first and  
+			second then update second  */
+		        else if (arr[i] > second && arr[i] != first) 
+		                second = arr[i]; 
+		} 
+		  
+		if (second == Integer.MIN_VALUE) 
+			System.out.print("There is no second largest" + " element\n"); 
+		else
+		        System.out.print("The second largest element" + " is " + second); 
+	} 
+	
+	// Driver code
+	public static void main(String[] args)
+	{
+		int arr[] = {12, 35, 1, 10, 34, 1};
+		int n = arr.length;
+		print2largest(arr, n);
 	}
-}
-
-System.out.printf("There is no second " +
-					"largest element\n");
-}
-
-// Driver code
-public static void main(String[] args)
-{
-int arr[] = {12, 35, 1, 10, 34, 1};
-int n = arr.length;
-print2largest(arr, n);
-}
 }
 
 
